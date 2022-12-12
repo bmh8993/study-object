095832decd5eee195ad0d6df8331b2042031d092
bmh8993 <bmh9175@gmail.com>
2022-12-13 08:11:57 +0900

    Bag의 자율성을 보장하라

--- zayden/src/main/kotlin/_01_ticket/Audience.kt
+++ zayden/src/main/kotlin/_01_ticket/Audience.kt
@@ -1,19 +1,9 @@
 package _01_ticket
 
 class Audience(
-    val bag: Bag
+    private val bag: Bag
 ) {
     fun buy(ticket: Ticket): Long {
-        return when (bag.hasInvitation()) {
-            true -> {
-                bag.ticket = ticket
-                0L
-            }
-            false -> {
-                bag.minusAmount(ticket.fee)
-                bag.ticket = ticket
-                ticket.fee
-            }
-        }
+        return bag.hold(ticket)
     }
 }
--- zayden/src/main/kotlin/_01_ticket/TicketSeller.kt
+++ zayden/src/main/kotlin/_01_ticket/TicketSeller.kt
@@ -1,10 +1,15 @@
 package _01_ticket
 
 class TicketSeller(
-    val ticketOffice: TicketOffice
+    private val ticketOffice: TicketOffice
 ) {
     // 수정 전: audience는 여전히 자율적인 존재가 아니다.
     // 수정 후: audience는 자율적인 존재가 되었다.
+
+    // ticketOffice에게 자율성을 부여하게 되면(ex. sellTicketTo(audience: Audience), 오히려 audience에게 의존하게 되고 결합도가 높아지게 된다.
+    // 트레이드 오프가 발생하는데, 이건 논의를 해봐야한다.
+    // 내 생각에는 아무래도 audience에게 의존하는 객체가 제일 적어야할 것 같다. 가장 중심이 되는 객체라고 생각된다.
+
     fun sellTo(audience: Audience) {
         ticketOffice.plusAmount(audience.buy(ticketOffice.getTicket()))
     }
