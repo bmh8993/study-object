f353ec6494afa1b2ddbd42beb08d2afe7aa7d450
bmh8993 <bmh9175@gmail.com>
2022-12-13 07:38:17 +0900

    theater의 결합도를 낮춰라

--- zayden/src/main/kotlin/_01_ticket/TicketSeller.kt
+++ zayden/src/main/kotlin/_01_ticket/TicketSeller.kt
@@ -2,4 +2,16 @@ package _01_ticket
 
 class TicketSeller(
     val ticketOffice: TicketOffice
-)
+) {
+    fun sellTo(audience: Audience) {
+        if (audience.bag.hasInvitation()) {
+            val ticket = ticketOffice.getTicket()
+            audience.bag.ticket = ticket
+        } else {
+            val ticket = ticketOffice.getTicket()
+            audience.bag.minusAmount(ticket.fee)
+            ticketOffice.plusAmount(ticket.fee)
+            audience.bag.ticket = ticket
+        }
+    }
+}
