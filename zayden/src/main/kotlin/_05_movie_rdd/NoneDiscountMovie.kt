db55674ce127dde1c3b80f4db6fd066a978a2e17
bmh8993 <bmh9175@gmail.com>
2023-01-05 00:46:43 +0900

    다형성으로 Movie를 개선하라

---
+++ zayden/src/main/kotlin/_05_movie_rdd/NoneDiscountMovie.kt
@@ -0,0 +1,15 @@
+package _05_movie_rdd
+
+import java.time.Duration
+
+class NoneDiscountMovie(
+    private val title: String,
+    private val runningTime: Duration,
+    override val fee: Money,
+    private val discountConditions: List<DiscountCondition>
+) : Movie(title, runningTime, fee, discountConditions) {
+
+    override fun calculateDiscountAmount(): Money {
+        return Money.ZERO
+    }
+}
